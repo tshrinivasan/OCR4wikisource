f59c533c154f4205d4bbd256b21f376039adb206
Kartik Mistry <kartik.mistry@gmail.com>
2018-08-03 16:29:36 +0800

    Fix pep8 formatting 3

--- find_missing.py
+++ find_missing.py
@@ -1,9 +1,9 @@
 import os.path
-start =  int(raw_input("start_no "))
+start = int(raw_input("start_no "))
 end = int(raw_input("end_no "))
 
-for i in range(start,end+1):
-	filename = "page_" + str(i).zfill(5)+ ".txt"
-	
-	if not os.path.isfile(filename):
-		print filename + " is misssing"
+for i in range(start, end+1):
+    filename = "page_" + str(i).zfill(5) + ".txt"
+
+    if not os.path.isfile(filename):
+        print filename + " is misssing"
